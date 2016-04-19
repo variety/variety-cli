2ad7502a9f53dba31b1b60bc097d051d90d8fc61
Tomas Dvorak <todvora@gmail.com>
2016-04-19 21:24:01 +0200

    better error messages (validation, execution of mongo), colored shell output, short arguments supported

--- spec/cli_spec.js
+++ spec/cli_spec.js
@@ -13,8 +13,12 @@ var getWritableStream = function() {
 
 var mockSpawn = function(bin, args) {
   var promise = Promise.resolve({'bin':bin, 'args':args});
-  // mock progress interface
-  promise.progress = function() {return this;};
+
+  promise.childProcess = {
+    stdout:{on:function(){}},
+    stderr:{on:function(){}}
+  };
+
   return promise;
 };
 
@@ -68,4 +72,87 @@ describe(__filename, function () {
     })
     .catch(done);
   });
+
+  it('CLI should forward auth arguments directly to mongo shell', function (done) {
+
+    var child = require('child-process-promise');
+
+    spyOn(child, 'spawn').andCallFake(mockSpawn);
+
+    var ws = getWritableStream();
+    cli({
+      stdout: ws,
+      stderr: ws,
+      exitFn: function(){},
+      argv:   ['node', 'variety-cli.js', 'foo/bar', '--username', 'lorem', '--password', 'ipsum', '--authenticationDatabase', 'my-auth-db'],
+      process: null
+    })
+    .then(function(res) {
+      expect(res.bin).toEqual('mongo');
+      expect(res.args[0]).toEqual('foo'); //db name
+      expect(res.args[1]).toEqual('--eval=var collection="bar";'); //variety args
+      expect(res.args[2]).toEndWith('variety.js'); //library path
+      expect(res.args[3]).toEqual('--username');
+      expect(res.args[4]).toEqual('lorem');
+      expect(res.args[5]).toEqual('--password');
+      expect(res.args[6]).toEqual('ipsum');
+      expect(res.args[7]).toEqual('--authenticationDatabase');
+      expect(res.args[8]).toEqual('my-auth-db');
+      done();
+    })
+    .catch(done);
+  });
+
+  it('CLI should handle short versions of mongo arguments (for example -u, -p)', function (done) {
+
+    var child = require('child-process-promise');
+
+    spyOn(child, 'spawn').andCallFake(mockSpawn);
+
+    var ws = getWritableStream();
+    cli({
+      stdout: ws,
+      stderr: ws,
+      exitFn: function(){},
+      argv:   ['node', 'variety-cli.js', 'foo/bar', '-u', 'lorem', '-p', 'ipsum', '--authenticationDatabase', 'my-auth-db'],
+      process: null
+    })
+    .then(function(res) {
+      expect(res.bin).toEqual('mongo');
+      expect(res.args[0]).toEqual('foo'); //db name
+      expect(res.args[1]).toEqual('--eval=var collection="bar";'); //variety args
+      expect(res.args[2]).toEndWith('variety.js'); //library path
+      expect(res.args[3]).toEqual('-u');
+      expect(res.args[4]).toEqual('lorem');
+      expect(res.args[5]).toEqual('-p');
+      expect(res.args[6]).toEqual('ipsum');
+      expect(res.args[7]).toEqual('--authenticationDatabase');
+      expect(res.args[8]).toEqual('my-auth-db');
+      done();
+    })
+    .catch(done);
+  });
+
+  it('CLI should handle incorrect args and print error + help', function (done) {
+
+    var child = require('child-process-promise');
+    spyOn(child, 'spawn').andCallFake(mockSpawn);
+
+    var ws = getWritableStream();
+    cli({
+      stdout: ws,
+      stderr: ws,
+      exitFn: function(){},
+      argv:   ['node', 'variety-cli.js', 'foo/bar', '--sort', '{foo:bar)'], // incorrect brackets pair
+      process: null
+    })
+    .then(function() {
+      done(new Error('Should throw exception and continue in catch branch'));
+    })
+    .catch(function(err) {
+      expect(err.message).toContain('Failed to parse option');
+      done();
+    });
+  });
+
 });
